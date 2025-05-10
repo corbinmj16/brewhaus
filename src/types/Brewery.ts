8ae49f37ec5562bab2af7137b97705edc5eef221
Corbin Jensen <corbin.jensen@me.com>
2025-05-09 20:25:38 -0400

    simple pagination, setup of home, calling api

---
+++ src/types/Brewery.ts
@@ -0,0 +1,16 @@
+export interface Brewery {
+  id: string
+  name: string
+  brewery_type: string
+  address_1: string
+  city: string
+  state_province: string
+  postal_code: string
+  country: string
+  longitude: number
+  latitude: number
+  phone: string
+  website_url: string
+  state: string
+  street: string
+}
