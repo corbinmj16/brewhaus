7ff9ec7dc361d039b373052efe070356609bbb65
Corbin Jensen <corbin.jensen@me.com>
2025-05-10 07:51:30 -0400

    brewery detail page

---
+++ src/views/BreweryDetailPage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const breweryId = route.params.id
+</script>
+
+<template>
+  <h1>{{ breweryId }}</h1>
+</template>
